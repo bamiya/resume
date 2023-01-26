08e4075a8f61a55431c3c8b5a78c4305ec53e8a5
Bamiya <bamiyamy@gmail.com>
2023-01-26 12:07:20 +0900

    헤더 css 작업중

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -4,6 +4,8 @@ const Header = () => {
   return (
     <Styled.Wrapper>
       <Styled.Text>testtext</Styled.Text>
+      <Styled.Text>testtext</Styled.Text>
+      <Styled.Text>testtext</Styled.Text>
     </Styled.Wrapper>
   );
 };
--- src/Component/Header/styled.js
+++ src/Component/Header/styled.js
@@ -2,18 +2,16 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   /* 레이아웃 */
-  display: flex;
+  display: flex; //가로방향 정렬
   position: fixed;
-  align-items: center;
-  justify-content: center;
+  align-items: center; // 수직(위아래)정렬에서 가운데 정렬, 반대로 justify-content: center; 는 수평정렬
   height: 60px;
   width: 100%;
-  top: 0px;
-  z-index: 5;
+  padding-left: 20px; //왼쪽 여백주기
 
   /* 색상 */
-  background: goldenrod;
-  color: white;
+  background: transparent;
+  color: black;
   border-bottom: 1px solid;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1), 0 3px 6px rgba(0, 0, 0, 0.2);
 `;
